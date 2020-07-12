f1b03fb23c803f7d3f78eeae175863f4e655c100
Le Vu Hoang Khanh <lvhkhanh@gmail.com>
2020-07-12 15:10:57 +0700

    need to review first controller

---
+++ demo2/src/main/kotlin/com/example/demo/controller/FirstController.kt
@@ -0,0 +1,13 @@
+package com.example.demo.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class FirstController {
+    @GetMapping("first")
+    fun first(model: Map<String, Any>): String {
+        model.plus(Pair("message", "message from controller"))
+        return "first"
+    }
+}
